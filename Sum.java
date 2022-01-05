ac31aef0f1b04cac055ecc203eff6fe266ebe15e
Ansh Galani <85383708+MrAnsh03@users.noreply.github.com>
2022-01-05 18:30:30 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,12 @@
+public class Sum
+{
+	public static void main(String[] args)
+	{
+		int a,b,c;
+		a=Integer.parseInt(args[0]);
+		b=Integer.parseInt(args[1]);
+		c=Integer.parseInt(args[2]);
+		int d=a+b+c;  
+		System.out.println("Sum is "+d);
+	}
+}

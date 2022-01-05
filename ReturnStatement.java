8a1bab98ebdb5ebd80be642fbba2af7b12ff7439
Ansh Galani <85383708+MrAnsh03@users.noreply.github.com>
2022-01-05 18:30:23 +0530

    Add files via upload

---
+++ ReturnStatement.java
@@ -0,0 +1,10 @@
+public class ReturnStatement
+{
+	public static void main(String[] args)
+	{
+		boolean t=true;
+		System.out.println("Before the Return");
+		if(t) return;
+		System.out.println("This Won't Execution");
+	}
+}
